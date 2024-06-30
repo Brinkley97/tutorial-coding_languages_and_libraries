42d808ac63ec9b471d465e8353ca49a0494d8470
Detravious J. Brinkley <d.brinkley97@gmail.com>
2024-06-30 07:53:40 -0400

    renaming files

--- py_torch/coursera/training_plots.py
+++ py_torch/coursera/training_plots.py
@@ -1,3 +1,5 @@
+# Base code from Deep Neural Networks with PyTorch by Joseph
+# https://www.coursera.org/learn/deep-neural-networks-with-pytorch
 # These are the libraries we are going to use in the lab.
 
 import torch
@@ -6,11 +8,13 @@ import numpy as np
 
 from mpl_toolkits import mplot3d
 
-# The class for plot the diagram
-
+# The class to help visualize the training process of ML models
 class plot_error_surfaces(object):
     
-    # Constructor
+    # Purpose: Generate and visualize the loss surface over a range of values for trainable params w and b
+    # Functionality:
+    #   Compute the loss surface Z over grid w and b
+    #   Plot the 3D loss surface and its contour map
     def __init__(self, w_range, b_range, X, Y, n_samples = 30, go = True):
         W = np.linspace(-w_range, w_range, n_samples)
         B = np.linspace(-b_range, b_range, n_samples)
@@ -47,14 +51,18 @@ class plot_error_surfaces(object):
             plt.contour(self.w, self.b, self.Z)
             plt.show()
     
-    # Setter
+    # Purpose: Store the params w and b and the corresponding loss for each iteration
+    # Functionality: Append the current values of w, b, loss to the respective lists W, B, LOSS
     def set_para_loss(self, W, B, loss):
         self.n = self.n + 1
         self.W.append(W)
         self.B.append(B)
         self.LOSS.append(loss)
     
-    # Plot diagram
+    # Purpose: Visualize the overall training process after all iterations
+    # Functionality:
+    #   Plot the 3D loss surface with the trajectory of w and b updates
+    #   Plot the contour of the loss surface with the trajectory of w and b updates
     def final_plot(self): 
         ax = plt.axes(projection = '3d')
         ax.plot_wireframe(self.w, self.b, self.Z)
@@ -66,7 +74,12 @@ class plot_error_surfaces(object):
         plt.ylabel('b')
         plt.show()
     
-    # Plot diagram
+    # Purpose: Visualize the training process at each steps (iteration)
+    # Functionality: Plot the data points and the estimated regression line based on 
+    # the current values of w and b
+    #   Plot the contour of the loss surface with the current position of w and b
+    #   Call method within the training loop to provide real-time feedback on the 
+    #   model's progress
     def plot_ps(self):
         plt.subplot(121)
         plt.ylim
