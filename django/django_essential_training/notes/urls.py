15fd12c604fc3bb92f179930719c8f80762334f9
Detravious J. Brinkley <d.brinkley97@gmail.com>
2024-10-21 23:32:11 -0400

    displaying a specific note

--- django/django_essential_training/notes/urls.py
+++ django/django_essential_training/notes/urls.py
@@ -3,5 +3,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-	path('notes', views.list),
+	path('notes', views.list), # See all notes
+    path('notes/<int:pk>', views.detail), # See specific note
 ]
--- django/django_essential_training/notes/views.py
+++ django/django_essential_training/notes/views.py
@@ -1,12 +1,19 @@
 from .models import Notes
 
+from django.http import Http404
 from django.shortcuts import render
 
 # Create your views here.
-
 def list(request):
 	all_notes = Notes.objects.all()
-  # render the original request (via user/HTML request),
-  # the name of the template (from [specific_app_folder]/[page_name].html), and 
-  # empty brackets or can use Django template language to pass into something to render to [page_name].html
+    # render the original request (via user/HTML request),
+    # the name of the template (from [specific_app_folder]/[page_name].html), and 
+    # empty brackets or can use Django template language to pass into something to render to [page_name].html
 	return render(request, 'notes/notes_list.html', {'notes': all_notes})
+
+def detail(request, pk):
+    try:
+        note_details = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note doesn't exist.")
+    return render(request, 'notes/notes_details.html', {'note': note_details})
