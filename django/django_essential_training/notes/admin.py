aca9b780b88f5b2a35300c5c338ed984b96b914a
Detravious J. Brinkley <d.brinkley97@gmail.com>
2024-10-21 23:13:44 -0400

    creating a html to show all notes

--- django/django_essential_training/notes/admin.py
+++ django/django_essential_training/notes/admin.py
@@ -1,3 +1,12 @@
+from . import models
+
 from django.contrib import admin
 
-# Register your models here.
+# Without below, Notes class won't be add to the django admin interface
+class NotesAdmin(admin.ModelAdmin):
+    # pass # Don't want any additional configuration on this admin model
+    list_display = ('title',) # In admin portal, go to Notes. Without, it'll say "Notes object (1)" and with, it'll say actual title of note
+
+# Attach Notes model to NotesAdmin model (which is attached to the ModelAdmin)
+admin.site.register(models.Notes, NotesAdmin)
+
--- django/django_essential_training/notes/views.py
+++ django/django_essential_training/notes/views.py
@@ -1,3 +1,12 @@
+from .models import Notes
+
 from django.shortcuts import render
 
 # Create your views here.
+
+def list(request):
+	all_notes = Notes.objects.all()
+  # render the original request (via user/HTML request),
+  # the name of the template (from [specific_app_folder]/[page_name].html), and 
+  # empty brackets or can use Django template language to pass into something to render to [page_name].html
+	return render(request, 'notes/notes_list.html', {'notes': all_notes})
