d67e2645f7603ed552cefc2bd86efe026a3cdcdc
Detravious J. Brinkley <d.brinkley97@gmail.com>
2024-10-23 21:52:22 -0400

    adding base html template

--- django/django_essential_training/notes/views.py
+++ django/django_essential_training/notes/views.py
@@ -4,23 +4,23 @@ from django.http import Http404
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
 
-"""Learning purposes: Function-based views
+# Learning purposes: Function-based views
 # Create your views here.
-def list(request):
-	all_notes = Notes.objects.all()
-    # render the original request (via user/HTML request),
-    # the name of the template (from [specific_app_folder]/[page_name].html), and 
-    # empty brackets or can use Django template language to pass into something to render to [page_name].html
-	return render(request, 'notes/notes_list.html', {'notes': all_notes})
+# def list(request):
+# 	all_notes = Notes.objects.all()
+#     # render the original request (via user/HTML request),
+#     # the name of the template (from [specific_app_folder]/[page_name].html), and 
+#     # empty brackets or can use Django template language to pass into something to render to [page_name].html
+# 	return render(request, 'notes/notes_list.html', {'notes': all_notes})
 
-def detail(request, pk):
-    try:
-        note_details = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("Note doesn't exist.")
-    return render(request, 'notes/notes_details.html', {'note': note_details})
+# def detail(request, pk):
+#     try:
+#         note_details = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         raise Http404("Note doesn't exist.")
+#     return render(request, 'notes/notes_details.html', {'note': note_details})
 
-"""
+
 
 # Class-based views: https://www.linkedin.com/learning-login/share?account=76870426&forceAccount=false&redirect=https%3A%2F%2Fwww.linkedin.com%2Flearning%2Fdjango-essential-training%2Fintroduction-to-django-class-based-views%3Ftrk%3Dshare_video_url%26shareId%3DxGUVf8a8QqeMF5b4gNVrvw%253D%253D
 
@@ -32,4 +32,4 @@ class NotesListView(ListView):
 class NotesDetailView(DetailView):
     model = Notes
     context_object_name = "note"
-    template_name = "notes/notes_detail.html"
+    template_name = "notes/notes_details.html"
