ce8630fb756e3631762ad929f4e26957322612a9
Detravious J. Brinkley <d.brinkley97@gmail.com>
2024-10-26 02:13:04 -0400

    cleaning code

--- django/django_essential_training/home/urls.py
+++ django/django_essential_training/home/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
 # Class-based views: https://www.linkedin.com/learning-login/share?account=76870426&forceAccount=false&redirect=https%3A%2F%2Fwww.linkedin.com%2Flearning%2Fdjango-essential-training%2Fintroduction-to-django-class-based-views%3Ftrk%3Dshare_video_url%26shareId%3DxGUVf8a8QqeMF5b4gNVrvw%253D%253D
 urlpatterns = [
     path('home', views.HomeView.as_view(), name='home.page'),
-    path('authorized', views.AuthorizedView.as_view())
+    path('authorized', views.AuthorizedView.as_view()),
 ]
